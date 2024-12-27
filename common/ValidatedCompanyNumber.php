0483a5bbf024b435c38df4bbe8684b1f45404548
Christof Moser <chm@metanet.ch>
2024-12-27 10:06:38 +0100

    Replace ValidatedEmailAddress::hasInvalidCharacter() by ValidatedEmailAddress::additionalSyntaxValidation() and ad CountryCodeEnum and ValidatedCompanyNumber classes.

---
+++ common/ValidatedCompanyNumber.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @author    Christof Moser
+ * @copyright Actra AG, Embrach, Switzerland, www.actra.ch
+ */
+
+namespace framework\common;
+
+use Exception;
+
+readonly class ValidatedCompanyNumber
+{
+	public bool $isValid;
+	public string $validatedNumber;
+
+	public function __construct(string $inputCompanyNumber, CountryCodeEnum $countryCodeEnum)
+	{
+		$this->isValid = match ($countryCodeEnum) {
+			CountryCodeEnum::CH => $this->validateCH(inputCompanyNumber: $inputCompanyNumber),
+			CountryCodeEnum::DK, CountryCodeEnum::NL => $this->validateWithNoSeparator(inputCompanyNumber: $inputCompanyNumber),
+			CountryCodeEnum::FI => $this->validateFI(inputCompanyNumber: $inputCompanyNumber),
+			CountryCodeEnum::NO => $this->validateNO(inputCompanyNumber: $inputCompanyNumber),
+			default => throw new Exception(message: 'To be implemented'),
+		};
+	}
+
+	private function validateCH(string $inputCompanyNumber): bool
+	{
+		if (preg_match(
+				pattern: '#^CHE-(?<group1>\d{3})[.\s]?(?<group2>\d{3})[.\s]?(?<group3>\d{3})$#',
+				subject: $inputCompanyNumber,
+				matches: $matches
+			) !== 1) {
+			return false;
+		}
+		$this->validatedNumber = 'CHE-' . implode(
+				separator: '.',
+				array: [
+					$matches['group1'],
+					$matches['group2'],
+					$matches['group3'],
+				]
+			);
+
+		return true;
+	}
+
+	private function validateNO(string $inputCompanyNumber): bool
+	{
+		if (preg_match(
+				pattern: '#^(?<group1>\d{3})[.\s]?(?<group2>\d{3})[.\s]?(?<group3>\d{3})$#',
+				subject: $inputCompanyNumber,
+				matches: $matches
+			) !== 1) {
+			return false;
+		}
+		$this->validatedNumber = implode(
+			separator: ' ',
+			array: [
+				$matches['group1'],
+				$matches['group2'],
+				$matches['group3'],
+			]
+		);
+
+		return true;
+	}
+
+	private function validateFI(string $inputCompanyNumber): bool
+	{
+		if (preg_match(
+				pattern: '#^(?<group1>\d{7})[.\s-]?(?<group2>\d{1})$#',
+				subject: $inputCompanyNumber,
+				matches: $matches
+			) !== 1) {
+			return false;
+		}
+		$this->validatedNumber = implode(
+			separator: '-',
+			array: [
+				$matches['group1'],
+				$matches['group2'],
+			]
+		);
+
+		return true;
+	}
+
+	private function validateWithNoSeparator(string $inputCompanyNumber): bool
+	{
+		if (preg_match(
+				pattern: '#^(?<group1>\d{8})$#',
+				subject: $inputCompanyNumber,
+				matches: $matches
+			) !== 1) {
+			return false;
+		}
+		$this->validatedNumber = $matches['group1'];
+
+		return true;
+	}
+}
