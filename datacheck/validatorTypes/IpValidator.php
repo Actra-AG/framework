f2afe486cf73bf1016ff4498d0b8ca34c039a063
Christof Moser <chm@metanet.ch>
2024-12-27 10:07:51 +0100

    BaseView: Add IpValidator::isInWhitelist() and accept ranges in ip whitelist.

--- datacheck/validatorTypes/IpValidator.php
+++ datacheck/validatorTypes/IpValidator.php
@@ -18,4 +18,38 @@ class IpValidator
 
 		return (filter_var(value: $input, filter: FILTER_VALIDATE_IP, options: $filterFlags) !== false);
 	}
+
+	public static function isInWhitelist(array $whiteList, string $ipAddressToCheck): bool
+	{
+		foreach ($whiteList as $whitelistItem) {
+			if ($ipAddressToCheck === $whitelistItem) {
+				return true;
+			}
+			if (!str_contains(
+				haystack: $whitelistItem,
+				needle: '/'
+			)) {
+				continue;
+			}
+			$tmp = explode(separator: '/', string: $whitelistItem);
+			$whitelistItem = $tmp[0];
+			$mask = $tmp[1];
+			// Sanitize IP
+			$ip1 = preg_replace(
+				pattern: '_(\d+\.\d+\.\d+\.\d+).*$_',
+				replacement: '$1',
+				subject: "$whitelistItem.0.0.0"
+			);
+			// Calculate range
+			$ip2 = long2ip(ip: ip2long(ip: $ip1) - 1 + (1 << (32 - $mask)));
+			if (
+				ip2long(ip: $ip1) <= ip2long(ip: $ipAddressToCheck)
+				&& ip2long(ip: $ip2) >= ip2long(ip: $ipAddressToCheck)
+			) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
